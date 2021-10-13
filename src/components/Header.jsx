150113b1227d307ea3263594d40ec792e5faf2fb
Sviatlana Vilchynskaya <kohechka1993@gmail.com>
2021-10-13 11:17:58 +0300

    Add components Header, Navbar, Profile. CSS.module

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import s from './Header.module.css';
+
+const Header = () => {
+  return (
+    <header className={s.header}>
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuqSUWGefeLef35q2txrO4W5gaQgjIrJoVvw&usqp=CAU" />
+    </header>
+  )
+}
+
+export default Header
