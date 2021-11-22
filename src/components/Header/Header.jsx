6dd60757eb3d06cf842a9db8dfc552bbe13f2ba4
Sviatlana Vilchynskaya <kohechka1993@gmail.com>
2021-11-22 13:40:14 +0300

    auth/me

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
-import s from './Header.module.css';
+import s from './Header.module.css'
+import { NavLink } from 'react-router-dom'
 
-const Header = () => {
+const Header = (props) => {
   return (
     <header className={s.header}>
       <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuqSUWGefeLef35q2txrO4W5gaQgjIrJoVvw&usqp=CAU" />
+      <div className={s.loginBlock}>
+        {props.isAuth ? props.login : <NavLink to={'/login'}>Login</NavLink>}
+      </div>
     </header>
   )
 }
--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -3,12 +3,14 @@ import dialogsReducer from './dialogs-reduser'
 import sidebarRedcer from './sidebar-reduser'
 import usersReducer from './users-reducer'
 import { createStore, combineReducers } from 'redux'
+import authReducer from './auth-reducer'
 
 let reducers = combineReducers({
   profilePage: profileReducer,
   dialogsPage: dialogsReducer,
   sidebar: sidebarRedcer,
   usersPage: usersReducer,
+  auth: authReducer,
 })
 
 let store = createStore(reducers)
