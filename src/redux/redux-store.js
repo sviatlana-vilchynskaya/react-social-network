9c4bfd750c008fafd36a85ca4e44dc4eeb0fff14
Sviatlana Vilchynskaya <kohechka1993@gmail.com>
2021-11-27 19:21:30 +0300

    redux-form, introduction

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -5,6 +5,7 @@ import usersReducer from './users-reducer'
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import authReducer from './auth-reducer'
 import thunkMiddleware from 'redux-thunk'
+import { reducer as formReducer } from 'redux-form'
 
 let reducers = combineReducers({
   profilePage: profileReducer,
@@ -12,6 +13,7 @@ let reducers = combineReducers({
   sidebar: sidebarRedcer,
   usersPage: usersReducer,
   auth: authReducer,
+  form: formReducer,
 })
 
 let store = createStore(reducers, applyMiddleware(thunkMiddleware))
