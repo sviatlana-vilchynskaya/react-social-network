5938d234e5fbd09956d3f37071dcbc404394d5d8
Sviatlana Vilchynskaya <kohechka1993@gmail.com>
2021-11-27 18:27:35 +0300

    componentDidUpdate

--- src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -25,6 +25,16 @@ class ProfileStatus extends React.Component {
     })
   }
 
+  componentDidUpdate(prevProps, prevState) {
+    if (prevProps.status !== this.props.status) {
+      this.setState({
+        status: this.props.status,
+      })
+    }
+
+    console.log('componentDidUpdate')
+  }
+
   render() {
     return (
       <div>
