3dbd6ad859f4427fbab5950e5152c14451df7a57
Sviatlana Vilchynskaya <kohechka1993@gmail.com>
2021-11-28 16:57:20 +0300

    redux-form: Dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,27 +3,20 @@ import s from './Dialogs.module.css'
 import DialogItem from './DialogItem/DialogItem'
 import Message from './Message/Message'
 import { Redirect } from 'react-router-dom'
+import { Field, reduxForm } from 'redux-form'
 
 const Dialogs = (props) => {
   let state = props.dialogsPage
-
   let dialogsElements = state.dialogs.map((d) => (
     <DialogItem name={d.name} key={d.id} id={d.id} />
   ))
-
   let messagesElements = state.messages.map((m) => (
     <Message message={m.message} key={m.id} />
   ))
 
   let newMessageBody = state.newMessageBody
-
-  let onSendMessageClick = () => {
-    props.sendMessage()
-  }
-
-  let onNewMessageChange = (e) => {
-    let body = e.target.value
-    props.updateNewMessageBody(body)
+  let addNewMessage = (values) => {
+    props.sendMessage(values.newMessageBody)
   }
 
   if (!props.isAuth) return <Redirect to={'/login'} />
@@ -34,20 +27,33 @@ const Dialogs = (props) => {
       <div className={s.messages}>
         <div>{messagesElements}</div>
         <div>
-          <div>
-            <textarea
-              value={newMessageBody}
-              onChange={onNewMessageChange}
-              placeholder="Enter your message"
-            ></textarea>
-          </div>
-          <div>
-            <button onClick={onSendMessageClick}>Send</button>
-          </div>
+          <AddMessageFormRedux onSubmit={addNewMessage} />
         </div>
       </div>
     </div>
   )
 }
+
+const AddMessageForm = (props) => {
+  return (
+    <form onSubmit={props.handleSubmit}>
+      <div>
+        <Field
+          // value={newMessageBody}
+          placeholder="Enter your message"
+          name="newMessageBody"
+          component={'textarea'}
+        />
+      </div>
+      <div>
+        <button>Send</button>
+      </div>
+    </form>
+  )
+}
+
+const AddMessageFormRedux = reduxForm({ form: 'dialogAddMessageForm' })(
+  AddMessageForm
+)
 
 export default Dialogs
--- src/redux/dialogs-reduser.js
+++ src/redux/dialogs-reduser.js
@@ -1,5 +1,4 @@
 const SEND_MESSAGE = 'SEND_MESSAGE'
-const UPDATE_NEW_MESSAGE_BODY = 'UPDATE_NEW_MESSAGE_BODY'
 
 let initialState = {
   dialogs: [
@@ -16,22 +15,14 @@ let initialState = {
     { id: 3, message: 'OK', name: 'Sveta' },
     { id: 4, message: 'Hello', name: 'Diana' },
   ],
-  newMessageBody: '',
 }
 
 const dialogsReducer = (state = initialState, action) => {
   switch (action.type) {
-    case UPDATE_NEW_MESSAGE_BODY:
-      return {
-        ...state,
-        newMessageBody: action.body,
-      }
-
     case SEND_MESSAGE:
-      let body = state.newMessageBody
+      let body = action.newMessageBody
       return {
         ...state,
-        newMessageBody: '',
         messages: [...state.messages, { id: 6, message: body }],
       }
 
@@ -40,10 +31,9 @@ const dialogsReducer = (state = initialState, action) => {
   }
 }
 
-export const sendMessageCreator = () => ({ type: SEND_MESSAGE })
-export const updateNewMessageBodyCreator = (body) => ({
-  type: UPDATE_NEW_MESSAGE_BODY,
-  body: body,
+export const sendMessageCreator = (newMessageBody) => ({
+  type: SEND_MESSAGE,
+  newMessageBody,
 })
 
 export default dialogsReducer
