01b60efa7b4f6e7353bddff8f8d866ba954e381e
Sviatlana Vilchynskaya <kohechka1993@gmail.com>
2021-11-28 17:24:43 +0300

    redux-form: MyPosts

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,6 +1,24 @@
 import React from 'react'
+import { Field, reduxForm } from 'redux-form'
 import s from './MyPosts.module.css'
 import Post from './Post/Post'
+
+const AddNewPostForm = (props) => {
+  return (
+    <form onSubmit={props.handleSubmit}>
+      <div>
+        <Field component="textarea" name="newPostText" />
+      </div>
+      <div>
+        <button>Add post</button>
+      </div>
+    </form>
+  )
+}
+
+const AddNewPostRedux = reduxForm({ form: 'ProfileAddNewPostForm' })(
+  AddNewPostForm
+)
 
 const MyPosts = (props) => {
   let postsElements = props.posts.map((p) => (
@@ -9,29 +27,15 @@ const MyPosts = (props) => {
 
   let newPostElement = React.createRef()
 
-  let onAddPost = () => {
-    props.addPost()
-  }
-
-  let onPostChange = () => {
-    let text = newPostElement.current.value
-    props.updateNewPostText(text)
+  let onAddPost = (values) => {
+    props.addPost(values.newPostText)
   }
 
   return (
     <div className={s.postsBlock}>
       <h3>My posts</h3>
       <div>
-        <div>
-          <textarea
-            onChange={onPostChange}
-            ref={newPostElement}
-            value={props.newPostText}
-          />
-        </div>
-        <div>
-          <button onClick={onAddPost}>Add post</button>
-        </div>
+        <AddNewPostRedux onSubmit={onAddPost} />
       </div>
       <div className={s.posts}>{postsElements}</div>
     </div>
