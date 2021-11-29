802101fff95c9f46b416f58fd6d559978766bed4
Sviatlana Vilchynskaya <kohechka1993@gmail.com>
2021-11-29 23:31:47 +0300

    mapStateToProps + selectors troubles

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -34,12 +34,15 @@ class ProfileContainer extends React.Component {
   }
 }
 
-let mapStateToProps = (state) => ({
-  profile: state.profilePage.profile,
-  status: state.profilePage.status,
-  authorizedUserId: state.auth.userId,
-  isAuth: state.auth.isAuth,
-})
+let mapStateToProps = (state) => {
+  //console.log('mapStateToProps PROFILE')
+  return {
+    profile: state.profilePage.profile,
+    status: state.profilePage.status,
+    authorizedUserId: state.auth.userId,
+    isAuth: state.auth.isAuth,
+  }
+}
 
 export default compose(
   connect(mapStateToProps, { getUserProfile, getStatus, updateStatus }),
