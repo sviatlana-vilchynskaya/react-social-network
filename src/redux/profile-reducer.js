94d7668ebfd5a5489e85fcb8ec1496850c03f99c
Sviatlana Vilchynskaya <kohechka1993@gmail.com>
2021-12-01 23:15:49 +0300

    redux-ducks + refactoring: reducers

--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -1,9 +1,9 @@
 import { profileAPI, usersAPI } from '../api/api'
 
-const ADD_POST = 'ADD-POST'
-const SET_USER_PROFILE = 'SET_USER_PROFILE'
-const SET_STATUS = 'SET_STATUS'
-const DELETE_POST = 'DELETE_POST'
+const ADD_POST = 'react-social-network/profile/ADD-POST'
+const SET_USER_PROFILE = 'react-social-network/profile/SET_USER_PROFILE'
+const SET_STATUS = 'react-social-network/profile/SET_STATUS'
+const DELETE_POST = 'react-social-network/profile/DELETE_POST'
 
 let initialState = {
   posts: [
@@ -57,24 +57,21 @@ export const setUserProfile = (profile) => ({ type: SET_USER_PROFILE, profile })
 export const setStatus = (status) => ({ type: SET_STATUS, status })
 export const deletePost = (postId) => ({ type: DELETE_POST, postId })
 
-export const getUserProfile = (userId) => (dispatch) => {
-  usersAPI.getProfile(userId).then((response) => {
-    dispatch(setUserProfile(response.data))
-  })
+export const getUserProfile = (userId) => async (dispatch) => {
+  const response = await usersAPI.getProfile(userId)
+  dispatch(setUserProfile(response.data))
 }
 
-export const getStatus = (userId) => (dispatch) => {
-  profileAPI.getStatus(userId).then((response) => {
-    dispatch(setStatus(response.data))
-  })
+export const getStatus = (userId) => async (dispatch) => {
+  const response = await profileAPI.getStatus(userId)
+  dispatch(setStatus(response.data))
 }
 
-export const updateStatus = (status) => (dispatch) => {
-  profileAPI.updateStatus(status).then((response) => {
-    if (response.data.resultCode === 0) {
-      dispatch(setStatus(status))
-    }
-  })
+export const updateStatus = (status) => async (dispatch) => {
+  const response = await profileAPI.updateStatus(status)
+  if (response.data.resultCode === 0) {
+    dispatch(setStatus(status))
+  }
 }
 
 export default profileReducer
--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -1,11 +1,14 @@
 import { usersAPI } from '../api/api'
-const FOLLOW = 'FOLLOW'
-const UNFOLLOW = 'UNFOLLOW'
-const SET_USERS = 'SET_USERS'
-const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE'
-const SET_TOTAL_USERS_COUNT = 'SET_TOTAL_USERS_COUNT'
-const TOGGLE_IS_FETCHING = 'TOGGLE_IS_FETCHING'
-const TOGGLE_IS_FOLLOWING_PROGRESS = 'TOGGLE_IS_FOLLOWING_PROGRESS'
+import { updateObjectInArray } from '../utils/object-helpers'
+
+const FOLLOW = 'react-social-network/users/FOLLOW'
+const UNFOLLOW = 'react-social-network/users/UNFOLLOW'
+const SET_USERS = 'react-social-network/users/SET_USERS'
+const SET_CURRENT_PAGE = 'react-social-network/users/SET_CURRENT_PAGE'
+const SET_TOTAL_USERS_COUNT = 'react-social-network/users/SET_TOTAL_USERS_COUNT'
+const TOGGLE_IS_FETCHING = 'react-social-network/users/TOGGLE_IS_FETCHING'
+const TOGGLE_IS_FOLLOWING_PROGRESS =
+  'react-social-network/users/TOGGLE_IS_FOLLOWING_PROGRESS'
 
 let initialState = {
   users: [],
@@ -21,21 +24,15 @@ const usersReducer = (state = initialState, action) => {
     case FOLLOW:
       return {
         ...state,
-        users: state.users.map((u) => {
-          if (u.id === action.userId) {
-            return { ...u, followed: true }
-          }
-          return u
+        users: updateObjectInArray(state.users, action.userId, 'id', {
+          followed: true,
         }),
       }
     case UNFOLLOW:
       return {
         ...state,
-        users: state.users.map((u) => {
-          if (u.id === action.userId) {
-            return { ...u, followed: false }
-          }
-          return u
+        users: updateObjectInArray(state.users, action.userId, 'id', {
+          followed: false,
         }),
       }
     case SET_USERS: {
@@ -85,38 +82,50 @@ export const toggleFollowingProgress = (isFetching, userId) => ({
 })
 
 export const requestUsers = (page, pageSize) => {
-  return (dispatch) => {
+  return async (dispatch) => {
     dispatch(toggleIsFetching(true))
     dispatch(setCurrentPage(page))
 
-    usersAPI.getUsers(page, pageSize).then((data) => {
-      dispatch(toggleIsFetching(false))
-      dispatch(setUsers(data.items))
-      dispatch(setTotalUsersCount(data.totalCount))
-    })
+    let data = await usersAPI.getUsers(page, pageSize)
+    dispatch(toggleIsFetching(false))
+    dispatch(setUsers(data.items))
+    dispatch(setTotalUsersCount(data.totalCount))
   }
 }
 
+const followUnfollowFlow = async (
+  dispatch,
+  userId,
+  apiMethod,
+  actionCreator
+) => {
+  dispatch(toggleFollowingProgress(true, userId))
+  let response = await apiMethod(userId)
+
+  if (response.data.resultCode == 0) {
+    dispatch(actionCreator(userId))
+  }
+  dispatch(toggleFollowingProgress(false, userId))
+}
+
 export const follow = (userId) => {
-  return (dispatch) => {
-    dispatch(toggleFollowingProgress(true, userId))
-    usersAPI.follow(userId).then((response) => {
-      if (response.data.resultCode == 0) {
-        dispatch(followSuccess(userId))
-      }
-      dispatch(toggleFollowingProgress(false, userId))
-    })
+  return async (dispatch) => {
+    followUnfollowFlow(
+      dispatch,
+      userId,
+      usersAPI.follow.bind(usersAPI),
+      followSuccess
+    )
   }
 }
 export const unfollow = (userId) => {
-  return (dispatch) => {
-    dispatch(toggleFollowingProgress(true, userId))
-    usersAPI.unfollow(userId).then((response) => {
-      if (response.data.resultCode == 0) {
-        dispatch(unfollow(userId))
-      }
-      dispatch(toggleFollowingProgress(false, userId))
-    })
+  return async (dispatch) => {
+    followUnfollowFlow(
+      dispatch,
+      userId,
+      usersAPI.unfollow.bind(usersAPI),
+      unfollowSuccess
+    )
   }
 }
 
